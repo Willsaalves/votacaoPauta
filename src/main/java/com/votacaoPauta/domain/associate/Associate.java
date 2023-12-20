fbd4d7939ee31e7f1bad33c9181b30486bf5c267
Willsaalves <will.saalves@gmail.com>
2023-12-20 19:15:35 -0300

    feat:atualizacao associate Entity

--- src/main/java/com/votacaoPauta/domain/associate/Associate.java
+++ src/main/java/com/votacaoPauta/domain/associate/Associate.java
@@ -1,40 +1,30 @@
 package com.votacaoPauta.domain.associate;
 
-import java.util.Set;
-
-import com.votacaoPauta.domain.schedule.Schedule;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "associate")
+@Entity
 @Table(name = "associate")
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
+
 public class Associate {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String firstName;
-	
-	@ManyToMany
-	@JoinTable(
-			name = "associate_schedule",
-            joinColumns = @JoinColumn(name = "associate_id"),
-            inverseJoinColumns = @JoinColumn(name = "schedule_id"))
-	private Set<Schedule>schedule;
+
+	private String nome;
 
 }
